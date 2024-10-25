8fb2d0942f835586ab20d74cece8195e26377f79
PROGRAMAÇÃO EM PYTHON 2024.4 <51129532024.4@senacrjedu.local>
2024-10-25 19:43:07 -0300

    versaoUm

---
+++ Correcao/exe.02.js
@@ -0,0 +1,5 @@
+//Construa uma página/programa onde o usuário digitará um número e o programa escreverá, na tela, o resultado do quadrado deste número. Atenção: utilizar apenas operadores básicos.
+
+let A = 10
+
+console.log(** A)
---
+++ Correcao/exe.05.js
@@ -0,0 +1,8 @@
+//Construa uma página/programa onde o usuário digitará o ano que nasceu e o programa irá escrever, na tela, quantos anos ele completará em 2040.
+
+let anoNascimento = 1981
+let anoQueTera = 2040
+
+let calculaAno = anoQueTera - anoNascimento
+
+console.log( calculaAno)
